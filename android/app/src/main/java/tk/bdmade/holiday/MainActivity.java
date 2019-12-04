21af76b182a525e8b19e783d1a8d97068f7e609d
hmtanbir <tanbir2025@gmail.com>
2019-12-04 13:05:15 +0600

    init project

---
+++ android/app/src/main/java/tk/bdmade/holiday/MainActivity.java
@@ -0,0 +1,13 @@
+package tk.bdmade.holiday;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
