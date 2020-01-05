9fb43568f473cba30baffb917865faf07e5b9b85
hmtanbir <tanbir2025@gmail.com>
2020-01-05 10:11:28 +0600

    adding dynamic data passing from one secreen to another screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,252 @@
+import 'dart:async';
+import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'package:holiday/home.dart';
-import 'package:holiday/splash_screen.dart';
-
-var routes = <String, WidgetBuilder> {
-  "/home": (BuildContext context) => Home(),
-};
+import 'package:holiday/model/month.dart';
+import 'package:holiday/model/holiday.dart';
 
 void main () => runApp(MaterialApp(
-  debugShowCheckedModeBanner: false,
-  home: SplashScreen(),
-  routes: routes,
-));
+    debugShowCheckedModeBanner: false,
+    theme: ThemeData(primaryColor: Color.fromRGBO(58, 66, 86, 1.0), fontFamily: 'IndieFlower'),
+    home: SplashScreen(),
+  ));
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  List<Month> monthList = [
+    Month(title: 'January', url: 'assets/json/1.json'),
+    Month(title: 'February', url: 'assets/json/2.json'),
+    Month(title: 'March', url: 'assets/json/3.json'),
+    Month(title: 'April', url: 'assets/json/4.json'),
+    Month(title: 'May', url: 'assets/json/5.json'),
+    Month(title: 'June', url: 'assets/json/6.json'),
+    Month(title: 'July', url: 'assets/json/7.json'),
+    Month(title: 'August', url: 'assets/json/8.json'),
+    Month(title: 'September', url: 'assets/json/9.json'),
+    Month(title: 'October', url: 'assets/json/10.json'),
+    Month(title: 'November', url: 'assets/json/11.json'),
+    Month(title: 'December', url: 'assets/json/12.json'),
+  ];
+
+  @override
+  void initState(){
+    super.initState();
+    Timer(Duration(seconds: 3),
+            ()=> Navigator.of(context).pushReplacement(MaterialPageRoute(
+              builder: (BuildContext context) => FirstScreen(months: monthList)
+            )));
+  }
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromRGBO(58, 66, 86, 1.0),
+      body: Center(
+        child: Text('Holidays',
+          textAlign: TextAlign.center,
+          style: TextStyle(fontSize: 50.0,
+          fontFamily: 'IndieFlower',
+          color: Colors.white),
+        ),
+      ),
+    );
+  }
+}
+
+class FirstScreen extends StatelessWidget {
+  final List<Month> months;
+  FirstScreen({Key key, @required this.months}) : super(key: key);
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Holidays', style: TextStyle(fontFamily: 'IndieFlower'),), centerTitle: true,),
+      backgroundColor: Color.fromRGBO(58, 66, 86, 1.0),
+      body: Container(
+        padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 2.0),
+        child: GridView.builder(
+          itemCount: months.length,
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
+            itemBuilder: (context, index) {
+              return Card(
+                  elevation: 10.0,
+                  margin: EdgeInsets.all(2.0),
+                  child: Container(
+                    decoration: BoxDecoration(color: Color.fromRGBO(64, 75, 96, .9)),
+                    child: InkWell(
+                      onTap: () {
+                        Navigator.push(context, MaterialPageRoute(builder: (context) => SecondScreen(month: months[index]),),);
+                        },
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        mainAxisSize: MainAxisSize.min,
+                        verticalDirection: VerticalDirection.down,
+                        children: <Widget>[
+                          SizedBox(height: 40.0),
+                          Center(
+                              child: Icon(
+                                Icons.calendar_today,
+                                size: 20.0,
+                                color: Colors.white,
+                              )),
+                          SizedBox(height: 20.0),
+                          new Center(
+                            child: new Text(months[index].title, style:TextStyle(fontSize: 16.0, color: Colors.white, fontFamily: 'IndieFlower')),
+                          )
+                        ],
+                      ),
+                    ),
+                  ));
+            },
+        ),
+      ),
+    );
+  }
+}
+
+class SecondScreen extends StatefulWidget {
+  // Declare a field that holds the Month.
+  final Month month;
+  // In the constructor, require a Month.
+  SecondScreen({Key key, @required this.month}) : super(key: key);
+
+  @override
+  _SecondScreenState createState() => _SecondScreenState();
+}
+
+class _SecondScreenState extends State<SecondScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: Text(widget.month.title,
+          style:
+          TextStyle(
+          fontFamily: 'IndieFlower'
+          )
+          ,
+        ),
+          centerTitle: true,),
+        backgroundColor: Color.fromRGBO(58, 66, 86, 1.0),
+        body: Container(
+          child: Center(
+            // Use future builder and DefaultAssetBundle to load the local JSON file
+            child: FutureBuilder(
+                future: DefaultAssetBundle.of(context).loadString(widget.month.url),
+                builder: (context, snapshot) {
+                  List<Holiday> holidays = parseJson(snapshot.data);
+                  return holidays.isEmpty? Center(child: CircularProgressIndicator()): HolidayList(holiday: holidays);
+                }),
+          ),
+        )
+    );
+  }
+
+  List<Holiday> parseJson(String response) {
+    if(response==null) {
+      return [];
+    }
+    final parsed = json.decode(response.toString()).cast<Map<String, dynamic>>();
+    return parsed.map<Holiday>((json) => Holiday.fromJson(json)).toList();
+  }
+}
+
+class HolidayList extends StatelessWidget {
+  final List<Holiday> holiday;
+  HolidayList({Key key, this.holiday}): super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+        itemCount: holiday == null ? 0 : holiday.length,
+        itemBuilder: (context, index) {
+          return Card(
+              elevation: 10.0,
+              margin: new EdgeInsets.symmetric(horizontal: 10.0, vertical: 6.0),
+              child: Container(
+                decoration: BoxDecoration(color: Color.fromRGBO(64, 75, 96, .9)),
+                child: ListTile(
+                  contentPadding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+                  leading: Container(padding: EdgeInsets.only(right: 12.0),decoration: BoxDecoration(border: Border(right: BorderSide(width: 1.0, color: Colors.white24))),
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 2.0),
+                      child: Column(
+                        children: <Widget>[
+                          Text(holiday[index].date, style: TextStyle(color: Colors.white, fontSize: 14.0, fontWeight: FontWeight.bold),),
+                          Text(holiday[index].day, style: TextStyle(color: Colors.white, fontSize: 14.0, fontWeight: FontWeight.bold),),
+                        ],
+                      ),
+                    ),
+                  ),
+                  title: Text(holiday[index].name,style: TextStyle(color: Colors.white, fontSize: 16.0, fontWeight: FontWeight.bold),),
+                  subtitle: Row(
+                    children: <Widget>[
+                      Expanded(
+                          flex: 1,
+                          child: Container(
+                            // tag: 'hero',
+                            child: LinearProgressIndicator(
+                                backgroundColor: Color.fromRGBO(209, 224, 224, 0.2),
+                                value: 1.0,
+                                valueColor: AlwaysStoppedAnimation(Color(int.parse(holiday[index].color)))),
+                          )),
+                      Expanded(
+                        flex: 4,
+                        child: Padding(
+                            padding: EdgeInsets.only(left: 10.0),
+                            child: Text(holiday[index].type,
+                                style: TextStyle(color: Colors.white))),
+                      )
+                    ],
+                  ),
+                  trailing:Icon(Icons.keyboard_arrow_right, color: Colors.white, size: 30.0),
+                  onTap: () {Navigator.push(context, MaterialPageRoute(builder: (context) => ThirdScreen(data: holiday[index]),),);},
+                ),
+              )
+          );
+        });
+  }
+
+}
+
+class ThirdScreen extends StatefulWidget {
+  final Holiday data;
+  ThirdScreen({Key key, @required this.data}) : super(key: key);
+  @override
+  _ThirdScreenState createState() => _ThirdScreenState();
+}
+
+class _ThirdScreenState extends State<ThirdScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.data.name, style: TextStyle(fontFamily: 'IndieFlower'),),
+        centerTitle: true,
+      ),
+      backgroundColor: Color.fromRGBO(58, 66, 86, 1.0),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Card(
+            elevation: 10.0,
+            margin: new EdgeInsets.symmetric(horizontal: 10.0, vertical: 6.0),
+            child: Container(
+              decoration: BoxDecoration(color: Color.fromRGBO(58, 66, 86, 1.0)),
+              child: ListTile(
+                contentPadding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+                subtitle: Padding(padding: EdgeInsets.only(left: 10.0),
+                    child: Text(widget.data.description,
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 16.0,
+                      ),
+                    )
+                ),
+              ),
+            )
+        ),
+      ),
+    );
+  }
+}
--- lib/my_navigator.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-class MyNavigator {
-  static void home(BuildContext context) {
-    print('pushNamed');
-    Navigator.pushNamed(context, "/home");
-  }
-}
